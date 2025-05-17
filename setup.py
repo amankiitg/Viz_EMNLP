d8265f53a03fc99c8ffaed30ae714e723741d97e
Aman Kesarwani <amank.iitg@gmail.com>
2025-05-17 06:32:20 -0400

    "Fix setup.py: minimal, no imports"

--- setup.py
+++ setup.py
@@ -1,12 +1,38 @@
-from setuptools import setup
+# from setuptools import setup
+
+# setup(
+#     name='Viz_EMNLP',
+#     version='',
+#     packages=['diffusion', 'diffusion.experiments'],
+#     url='',
+#     license='',
+#     author='amankesarwani',
+#     author_email='',
+#     description=''
+# )
+
+from pathlib import Path
+from setuptools import setup, find_packages
+
+this_dir = Path(__file__).parent
 
 setup(
-    name='Viz_EMNLP',
-    version='',
-    packages=['diffusion', 'diffusion.experiments'],
-    url='',
-    license='',
-    author='amankesarwani',
-    author_email='',
-    description=''
+    name="viz_emnlp",
+    version="0.0.1",
+    description="Diffusion-model experiments for EMNLP visualizations",
+    long_description=(this_dir / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+    packages=find_packages(include=["diffusion*", "project*"]),
+    install_requires=[
+        "torch",
+        "diffusers",
+        "accelerate",
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        # add any others you import
+    ],
+    include_package_data=True,
 )
+
